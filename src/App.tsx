39a557a7369c215cc32e9db2aad8d1a3269283b5
Hervé Zossou <hervezossou8@gmail.com>
2025-01-30 19:50:26 +0100

    Update dependenciew with prettier plugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 function App() {
-
   return (
-    <div>
-      <h1>React TypeScript App</h1>
+    <div className="mx-auto bg-blue-500 p-4">
+      <h1 className="font-sans text-2xl font-bold">React TypeScript App</h1>
+      <p className="text-lg">Hello</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
