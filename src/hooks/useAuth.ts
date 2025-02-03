42b94f436c48225cb217aeadfc19bef9186ce708
Hervé Zossou <hervezossou8@gmail.com>
2025-02-03 16:28:00 +0100

    feat(contexts): rename folder name to contexts

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { AuthContext } from "../context/AuthContext";
+import { AuthContext } from "../contexts/AuthContext";
 
 // Hook personnalisé pour éviter le problème de Fast Refresh
 export const useAuth = () => {
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import "./styles/globals.css"
 import ErrorBoundary from './components/ErrorBoundary.tsx'
-import { AuthProvider } from './context/AuthProvider.tsx'
+import { AuthProvider } from './contexts/AuthProvider.tsx'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
