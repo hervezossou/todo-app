48e08a48e6e08bf74ad548a6673914cb6b55da85
Hervé Zossou <hervezossou8@gmail.com>
2025-02-11 21:06:31 +0100

    feat(logout): add logout feature to the dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,14 @@
+import { useAuth } from "../hooks/useAuth"
+
 export default function Dashboard() {
+    const { logout } = useAuth()
+
     return (
-        <main>
-            
+        <main className="p-6">
+            <h1 className="text-3xl font-bold">Welcome to your dashboard</h1>
+            <button className="text-red-600" onClick={logout}>
+                Log out
+            </button>
         </main>
     )
 }
