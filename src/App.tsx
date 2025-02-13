3e160cd8c91df6120aab40650fe84cf5df0beeca
Hervé Zossou <hervezossou8@gmail.com>
2025-02-13 10:05:08 +0100

    feat(Dashboard): move fetchTasks from App to Dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import { useEffect } from "react";
-import supabase from "./services/supabase";
 import { BrowserRouter as Router, Routes, Route } from "react-router";
 import Dashboard from "./pages/Dashboard";
 import Onboarding from "./pages/Onboarding";
@@ -8,18 +6,6 @@ import Register from "./pages/Register";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 export default function App() {
-  useEffect(() => {
-    const fetchTasks = async () => {
-      const { data, error } = await supabase.from("tasks").select("*");
-      if (error) {
-        console.error("Supabase error:", error);
-      } else {
-        console.log("Tasks", data);
-      }
-    };
-
-    fetchTasks();
-  }, []);
 
   return (
     <Router>
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,12 +1,27 @@
-import { useAuth } from "../hooks/useAuth"
+import { useEffect } from "react";
+import { useAuth } from "../hooks/useAuth";
+import supabase from "../services/supabase";
 
 export default function Dashboard() {
     const { logout } = useAuth()
 
+    useEffect(() => {
+        const fetchTasks = async () => {
+          const { data, error } = await supabase.from("tasks").select("*");
+          if (error) {
+            console.error("Supabase error:", error);
+          } else {
+            console.log("Tasks", data);
+          }
+        };
+    
+        fetchTasks();
+}, []);
+
     return (
-        <main className="p-6">
-            <h1 className="text-3xl font-bold">Welcome to your dashboard</h1>
-            <button className="text-red-600" onClick={logout}>
+        <main className="bg-neutral-800 min-h-screen p-6">
+            <h1 className="text-3xl text-white font-bold">Welcome to your dashboard</h1>
+            <button className="text-red-600 hover:underline hover:cursor-pointer" onClick={logout}>
                 Log out
             </button>
         </main>
