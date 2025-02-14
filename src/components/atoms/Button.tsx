60ad8dcab479d64dd0e9cf9f8a7a00dac009c5ea
Hervé Zossou <hervezossou8@gmail.com>
2025-02-14 11:27:31 +0100

    style(Button): add cursor pointer on hover for Button component

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -13,8 +13,8 @@ interface ButtonProps {
 export default function Button({ label, ariaLabel, variant = "primary", disabled = false, icon, onClick }: ButtonProps) {
     const baseStyles = "flex items-center justify-center gap-2 p-3 font-semibold rounded-md focus:outline-none transition";
     const variantStyles = {
-        primary: `w-full ${disabled ? "bg-blue-300 cursor-not-allowed" : "bg-blue-600 hover:bg-blue-700"} text-white`,
-        secondary: `w-32 h-10 ${disabled ? "bg-gray-300 cursor-not-allowed" : "bg-gray-200 hover:bg-gray-300"} text-black`,
+        primary: `w-full ${disabled ? "bg-blue-300 cursor-not-allowed" : "bg-blue-600 hover:bg-blue-700 hover:cursor-pointer"} text-white`,
+        secondary: `w-32 h-10 ${disabled ? "bg-gray-300 cursor-not-allowed" : "bg-gray-200 hover:bg-gray-300 hover:cursor-pointer"} text-black`,
         danger: "w-32 h-10 bg-red-300 text-red-600 hover:bg-red-400 hover:text-red-700 hover:cursor-pointer"
     }
     return (
